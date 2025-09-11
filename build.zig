09d22a6ba02439960d1e014dab5610b4980b45cc
Ari Johannesson <arividar@gmail.com>
2025-09-11 08:54:06 +0000

    test: add neighborCount/stepGrid with unit tests; refactor main to use stepGrid

--- build.zig
+++ build.zig
@@ -4,11 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "GoLChatGPT",
-        .root_source_file = .{ .path = "GoLChatGPT.zig" },
+    const root_mod = b.createModule(.{
+        .root_source_file = b.path("GoLChatGPT.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "GoLChatGPT",
+        .root_module = root_mod,
     });
 
     b.installArtifact(exe);
@@ -21,13 +25,10 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "GoLChatGPT.zig" },
-        .target = target,
-        .optimize = optimize,
+        .root_module = root_mod,
     });
     const run_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_tests.step);
 }
-
